3521952e2275cbd92f9ae53c2c4ec7bf151f8d8b
General Kenobi <78252449+mmgleave@users.noreply.github.com>
2021-07-05 21:38:12 -0600

    comments

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,6 +4,12 @@ const pizzaController = {
     // get all pizzas
     getAllPizza(req, res) {
         Pizza.find({})
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => {
                 console.log(err);
@@ -14,18 +20,23 @@ const pizzaController = {
     // get one pizza by id
     getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
-        .then(dbPizzaData => {
-            // if no pizza is found, send 404
-            if(!dbPizzaData) {
-                res.status(404).json({ message: 'No pizza found with this id!' });
-                return;
-            }
-            res.json(dbPizzaData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbPizzaData => {
+                // if no pizza is found, send 404
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
     },
 
     // create pizza
@@ -39,8 +50,8 @@ const pizzaController = {
     updatePizza({ params, body }, res) {
         Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
             .then(dbPizzaData => {
-                if(!dbPizzaData) {
-                    res.status(404).json({ message: 'No pizza found with this id!'});
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
                     return;
                 }
                 res.json(dbPizzaData);
@@ -52,8 +63,8 @@ const pizzaController = {
     deletePizza({ params }, res) {
         Pizza.findOneAndDelete({ _id: params.id })
             .then(dbPizzaData => {
-                if(!dbPizzaData) {
-                    res.status(404).jeson({ message: 'No pizza found with this id!'});
+                if (!dbPizzaData) {
+                    res.status(404).jeson({ message: 'No pizza found with this id!' });
                     return;
                 }
                 res.json(dbPizzaData);
